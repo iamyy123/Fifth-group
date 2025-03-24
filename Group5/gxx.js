c610f0b818c654fe572c475b3c57b90863906e80
183x7 <2757153021@.com>
2025-03-24 21:33:08 +0800

    gxx新加的代码

---
+++ Group5/gxx.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = () => {
+    return (
+        <div className="bg-gray-100 p-8 rounded-md shadow-md text-center">
+            <h1 className="text-2xl font-bold text-blue-600">欢迎来到我的 React 应用！</h1>
+            <p className="text-gray-700 mt-2">我是第五组成员，我爱我的组长！!嘻嘻。</p>
+        </div>
+    );
+};
+
+export default Welcome;    
