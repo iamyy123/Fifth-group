757dc5774abf74794c1f67722186337e28bc784b
LUOHONGYU0 <2188802031@qq.com>
2025-03-24 21:52:52 +0800

    lhy
    罗鸿予新加代码

---
+++ Group5/lhy.js
@@ -0,0 +1,13 @@
+function App() {
+
+    return (
+        <div className="flex flex-row space-x-4 p-4">
+   <div className="bg-blue-200 p-4 ">元素 1</div>
+   <div className="bg-green-200 p-4 font-mono font-bold text-blue-600 text-2xl">元素 2222222</div>
+   <div className="bg-red-200 p-4">元素 3</div>
+ </div>
+    );
+
+}
+
+export default App;
